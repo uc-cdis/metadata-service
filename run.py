6ba0ddce6f50f6171e95345e5a15da0466cc7e3c
John McCann <johnmccann@uchicago.edu>
2020-10-27 12:06:54 -0700

    chore(logging): dictConfig("mds") in __init__.py

--- run.py
+++ run.py
@@ -8,17 +8,12 @@ import os
 import sys
 import uvicorn
 import yaml
-import logging.config
+import copy
 
 from mds.main import get_app
-import mds.config
+from mds.config import DEFAULT_LOGGING_CONFIG
 
-#  from mds.config import LOGGING_CONFIG as MDS_LOGGING_CONFIG
-
-#  logging.config.dictConfig(mds.config.LOGGING_CONFIG)
-error_and_access_log_config = dict(
-    version=1,
-    disable_existing_loggers=False,
+UVICORN_LOGGERS_CONFIG = dict(
     loggers={
         "uvicorn": {
             "level": "INFO",
@@ -40,18 +35,8 @@ error_and_access_log_config = dict(
         },
     },
 )
-#  XXX deepcopy
-uvicorn_logging_config = mds.config.LOGGING_CONFIG.copy()
-uvicorn_logging_config["loggers"].update(error_and_access_log_config["loggers"])
-#  logconfig_dict = gunicorn_logging_config
-
-#  logging_config.update(mds.config.LOGGING_CONFIG)
-logging.config.dictConfig(uvicorn_logging_config)
-#  logging.config.dictConfig(error_and_access_log_config)
-
-
-print("configured logging from run.py!")
-
+logging_config = copy.deepcopy(DEFAULT_LOGGING_CONFIG)
+logging_config.update(UVICORN_LOGGERS_CONFIG)
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -65,4 +50,4 @@ if __name__ == "__main__":
             yaml.dump(schema, f, default_flow_style=False)
         print(f"Saved docs at {path}")
     else:
-        uvicorn.run("mds.asgi:app", reload=True, log_config=uvicorn_logging_config)
+        uvicorn.run("mds.asgi:app", reload=True, log_config=logging_config)
--- src/mds/__init__.py
+++ src/mds/__init__.py
@@ -1,7 +1,11 @@
-import logging
+import copy
+import logging.config
+
 from . import config
 
-logger = logging.getLogger("mds")
 
-if config.DEBUG:
-    logger.setLevel(logging.DEBUG)
+_logging_config = copy.deepcopy(config.DEFAULT_LOGGING_CONFIG)
+_logging_config.update(config.MDS_LOGGER_CONFIG)
+logging.config.dictConfig(_logging_config)
+
+logger = logging.getLogger("mds")
