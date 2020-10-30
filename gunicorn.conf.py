06f871a4a2905b737334fd971a3079c44265d940
John McCann <johnmccann@uchicago.edu>
2020-10-30 15:08:51 -0700

    chore(logging): use DEBUG env for gunicorn/uvicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,8 @@ import logging
 import gunicorn.glogging
 import cdislogging
 
+import mds.config
+
 
 class CDISLogger(gunicorn.glogging.Logger):
     """
@@ -24,10 +26,12 @@ class CDISLogger(gunicorn.glogging.Logger):
         super().__init__(cfg)
 
         self._remove_handlers(logging.getLogger())
-        cdislogging.get_logger(None, log_level="warn")
+        cdislogging.get_logger(None, log_level="debug" if mds.config.DEBUG else "warn")
         for logger_name in ["gunicorn", "gunicorn.error", "gunicorn.access"]:
             self._remove_handlers(logging.getLogger(logger_name))
-            cdislogging.get_logger(logger_name, log_level="info")
+            cdislogging.get_logger(
+                logger_name, log_level="debug" if mds.config.DEBUG else "info"
+            )
 
 
 logger_class = CDISLogger
