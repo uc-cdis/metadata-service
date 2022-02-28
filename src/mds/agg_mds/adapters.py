77778a21dc97f2c2cfdc7084a2205087cce51b3a
xritter1 <67650599+xritter1@users.noreply.github.com>
2022-02-28 14:17:45 -0800

    Update adapters.py

--- src/mds/agg_mds/adapters.py
+++ src/mds/agg_mds/adapters.py
@@ -246,10 +246,8 @@ class ISCPSRDublin(RemoteMetadataAdapter):
             else:
                 results = mapped_fields
 
-        if isinstance(results.get("investigators"), list):
+        if isinstance(results["investigators"], list):
             results["investigators"] = ",".join(results["investigators"])
-        if isinstance(results.get("investigators_name"), list):
-            results["investigators_name"] = ",".join(results["investigators_name"])
         return results
 
     def normalizeToGen3MDSFields(self, data, **kwargs) -> Dict[str, Any]:
@@ -502,10 +500,8 @@ class PDAPS(RemoteMetadataAdapter):
             else:
                 results = mapped_fields
 
-        if isinstance(results.get("investigators"), list):
+        if isinstance(results["investigators"], list):
             results["investigators"] = results["investigators"].join(", ")
-        if isinstance(results.get("investigators_name"), list):
-            results["investigators_name"] = results["investigators_name"].join(", ")
         return results
 
     def normalizeToGen3MDSFields(self, data, **kwargs) -> Dict[str, Any]:
