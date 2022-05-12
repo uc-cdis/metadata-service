689be712bb1ba9010b0803baf04a19659075e4c6
George Thomas <george42@uchicago.edu>
2022-05-12 15:41:43 -0700

    Handle single quotes in resource paths, clean up use of default authz

--- migrations/versions/4d93784a25e5_add_authz_column.py
+++ migrations/versions/4d93784a25e5_add_authz_column.py
@@ -5,10 +5,11 @@ Revises: f96cb3b2c523
 Create Date: 2022-04-19 10:30:03.967205
 
 """
+import json
+
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
-import json
 
 from mds.config import DEFAULT_AUTHZ_STR
 
@@ -18,12 +19,15 @@ down_revision = "f96cb3b2c523"
 branch_labels = None
 depends_on = None
 
-DEFAULT_RESOURCE_PATHS = ["/open"]
+
+def escape(str):
+    # escape single quotes for SQL statement
+    return str.replace("'", "''")
 
 
 def upgrade():
     # migrate the authz data from the `data` column and remove some metadata fields
-    required_authz_keys = ["version", "_resource_paths"]
+    authz_key = "_resource_paths"
     remove_metadata_keys = ["_uploader_id", "_filename", "_bucket", "_file_extension"]
 
     # add the new `authz` column (nullable for now)
@@ -38,16 +42,14 @@ def upgrade():
         guid, data = r[0], r[1]
         # default values for authz (["/open"])
         authz_data = json.loads(DEFAULT_AUTHZ_STR)
-        # check for any authz values in data column
-        if data is not None:
-            for authz_key in required_authz_keys:
-                if authz_key in data.keys():
-                    authz_data[authz_key] = data.pop(authz_key)
+        # check for any existing authz-resource-paths in the `data` column
+        if data is not None and authz_key in data:
+            authz_data[authz_key] = data.pop(authz_key)
             # scrub internal fields from metadata
             for metadata_key in remove_metadata_keys:
                 if metadata_key in data.keys():
                     data.pop(metadata_key)
-            sql_statement = f"UPDATE metadata SET authz='{json.dumps(authz_data)}', data='{json.dumps(data)}' WHERE guid='{guid}'"
+            sql_statement = f"UPDATE metadata SET authz='{escape(json.dumps(authz_data))}', data='{json.dumps(data)}' WHERE guid='{guid}'"
         else:
             sql_statement = f"UPDATE metadata SET authz='{json.dumps(authz_data)}' WHERE guid='{guid}'"
         connection.execute(sql_statement)
@@ -62,10 +64,7 @@ def downgrade():
     # get the default resource path from config
     authz_key = "_resource_paths"
     default_authz = json.loads(DEFAULT_AUTHZ_STR)
-    if authz_key in default_authz:
-        default_paths = default_authz[authz_key]
-    else:
-        default_paths = DEFAULT_RESOURCE_PATHS
+    default_paths = default_authz.get(authz_key, ["/open"])
 
     connection = op.get_bind()
     results = connection.execute("SELECT guid, authz, data from metadata").fetchall()
@@ -76,14 +75,12 @@ def downgrade():
         data = r[2]
 
         # keep only the values that were stored pre-upgrade (ie, keep non-default resource paths)
-        if authz_key in authz_data.keys() and authz_data[authz_key] != default_paths:
+        if authz_data.get(authz_key) != default_paths:
             if data is None:
                 data = {authz_key: authz_data.pop(authz_key)}
             else:
                 data[authz_key] = authz_data.pop(authz_key)
-            sql_statement = (
-                f"UPDATE metadata SET data='{json.dumps(data)}' WHERE guid='{guid}'"
-            )
+            sql_statement = f"UPDATE metadata SET data='{escape(json.dumps(data))}' WHERE guid='{guid}'"
             connection.execute(sql_statement)
 
     # drop the `authz` column
