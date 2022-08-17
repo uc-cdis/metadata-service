08e543634c9270b5aef002c70b69299cfd43dfd7
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2022-08-17 13:04:45 -0500

    fix "is" warning

--- src/mds/agg_mds/datastore/elasticsearch_dao.py
+++ src/mds/agg_mds/datastore/elasticsearch_dao.py
@@ -97,7 +97,7 @@ def normalize_field(doc, key, normalize_type):
     try:
         if normalize_type == "object" and isinstance(doc[key], str):
             value = doc[key]
-            doc[key] = None if value is "" else json.loads(value)
+            doc[key] = None if value == "" else json.loads(value)
         if normalize_type == "number" and isinstance(doc[key], str):
             doc[key] = None
         if normalize_type == "date" and isinstance(doc[key], str) and doc[key] == "":
