e6e9a04e76ad7e1118c704c930f35b1a5362efc7
Craig Barnes <craigrbarnes@uchicago.edu>
2022-10-12 18:51:22 -0500

    update FastAPI documentation

--- src/mds/agg_mds/query.py
+++ src/mds/agg_mds/query.py
@@ -2,19 +2,41 @@ from fastapi import HTTPException, Query, APIRouter, Request
 from starlette.status import HTTP_404_NOT_FOUND
 from mds import config
 from mds.agg_mds import datastore
+from typing import Any, Dict, List
+from pydantic import BaseModel
 
 mod = APIRouter()
 
 
 @mod.get("/aggregate/commons")
 async def get_commons():
-    """Returns a list of all registered commons"""
+    """Returns a list of all commons with data in the aggregate metadata-service
+
+    Example:
+
+        { commons: ["commonsA", "commonsB" ] }
+
+    """
     return await datastore.get_commons()
 
 
 @mod.get("/aggregate/info/{what}")
-async def get_commons(what: str):
-    """Returns information from the aggregate metadata service."""
+async def get_commons_info(what: str):
+    """Returns status and configuration information about aggregate metadata service.
+
+    Return configuration information. Currently supports only 1 information type:
+    **schema**
+
+    Example:
+
+        {
+          schema: {
+                ...
+                ...
+                }
+        }
+
+    """
     res = await datastore.get_commons_attribute(what)
     if res:
         return res
@@ -26,14 +48,18 @@ async def get_commons(what: str):
 
 
 @mod.get("/aggregate/metadata")
-async def metadata(
+async def get_aggregate_metadata(
     _: Request,
     limit: int = Query(
         20, description="Maximum number of records returned. (e.g. max: 2000)"
     ),
     offset: int = Query(0, description="Return results at this given offset."),
     counts: str = Query(
-        "", description="Return count of a field instead of the value."
+        "",
+        description="Return count of a field instead of the value if field is an array\
+           otherwise field is unchanged. If field is **null** will set field to **0**.\
+           Multiple fields can be compressed by comma separating the field names:\
+           **files,authors**",
     ),
     flatten: bool = Query(
         False, description="Return the results without grouping items by commons."
@@ -44,19 +70,10 @@ async def metadata(
 ):
     """Returns metadata records
 
-    The pagination option adds a pagination object to the response:
-    {
-      "commonA" : {
-          ... Metadata
-      },
-       "commonB" : {
-          ... Metadata
-      }
-      ...
-    }
+    Returns medata records namespaced by commons as a JSON object.
+    Example without pagination:
 
         {
-        results: {
           "commonA" : {
               ... Metadata
           },
@@ -64,16 +81,30 @@ async def metadata(
               ... Metadata
           }
           ...
-        },
-        "pagination": {
-            "hits": 64,
-            "offset": 0,
-            "pageSize": 20,
-            "pages": 4
         }
-    }
+
+    The pagination option adds a pagination object to the response:
+
+        {
+            results: {
+              "commonA" : {
+                  ... Metadata
+              },
+               "commonB" : {
+                  ... Metadata
+              }
+              ...
+            },
+            "pagination": {
+                "hits": 64,
+                "offset": 0,
+                "pageSize": 20,
+                "pages": 4
+            }
+        }
 
     The flatten option removes the commons' namespace so all results are a child or results:
+
         results: {
               ... Metadata from commons A
               ... Metadata from commons B
@@ -84,7 +115,7 @@ async def metadata(
 
     The counts options when applied to an array or dictionary will replace
     the field value with its length. If the field values is None it will replace it with 0.
-    All other type will be unchanged.
+    All other types will be unchanged.
     """
     results = await datastore.get_all_metadata(limit, offset, counts, flatten)
     if pagination is False:
@@ -93,8 +124,21 @@ async def metadata(
 
 
 @mod.get("/aggregate/metadata/{name}")
-async def metadata_name(name: str):
-    """Returns the all the metadata from the named commons."""
+async def get_aggregate_metadata_for_commons(
+    name: str = Query(
+        False, description="Return the results without grouping items by commons."
+    )
+):
+    """et all metadata records from a commons by name
+
+    Returns an array containing all the metadata entries for a single commons.
+    There are no limit/offset parameters.
+
+    Example:
+
+        [ { id2: { name: "bear" } } , { id3: { name: "cat" } }]
+
+    """
     res = await datastore.get_all_named_commons_metadata(name)
     if res:
         return res
@@ -106,9 +150,30 @@ async def metadata_name(name: str):
 
 
 @mod.get("/aggregate/tags")
-async def metadata_tags():
-    """
-    Returns the tags associated with the named commons.
+async def get_aggregate_tags():
+    """Returns aggregate category, name and counts across all commons
+
+    Example:
+
+            {
+              "Data Type": {
+                "total": 275,
+                "names": [
+                  {
+                    "Genotype": 103,
+                    "Clinical Phenotype": 100,
+                    "DCC Harmonized": 24,
+                    "WGS": 20,
+                    "SNP/CNV Genotypes (NGS)": 6,
+                    "RNA-Seq": 5,
+                    "WXS": 5,
+                    "Targeted-Capture": 3,
+                    "miRNA-Seq": 3,
+                    "CNV Genotypes": 2
+                  }
+                ]
+              }
+            }
     """
     res = await datastore.get_all_tags()
     if res:
@@ -121,9 +186,14 @@ async def metadata_tags():
 
 
 @mod.get("/aggregate/metadata/{name}/info")
-async def metadata_info(name: str):
+async def get_aggregate_metadata_commons_info(name: str):
     """
     Returns information from the named commons.
+
+    Example:
+
+        { commons_url: "gen3.datacommons.io" }
+
     """
     res = await datastore.get_commons_attribute(name)
     if res:
@@ -136,8 +206,13 @@ async def metadata_info(name: str):
 
 
 @mod.get("/aggregate/metadata/guid/{guid:path}")
-async def metadata_name_guid(guid: str):
-    """Get the metadata of the GUID in the named commons."""
+async def get_aggregate_metadata_guid(guid: str):
+    """Returns a metadata record by GUID
+
+    Example:
+
+         { id2: { name: "bear" } }
+    """
     res = await datastore.get_by_guid(guid)
     if res:
         return res
