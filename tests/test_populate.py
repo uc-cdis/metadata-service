c7f1f3e1f40287f5d49b458ce03f98e5b895f824
Mingfei Shao <mshao1@uchicago.edu>
2023-01-31 16:47:10 -0600

    wip: fix test

--- tests/test_populate.py
+++ tests/test_populate.py
@@ -275,7 +275,7 @@ async def test_populate_config():
             )
         config = parse_config_from_file(Path(fp.name))
         await populate_config(config)
-        mock_datastore.update_config_info.called_with(["_subjects_count"])
+        await mock_datastore.update_config_info.called_with(["_subjects_count"])
 
 
 @pytest.mark.asyncio
