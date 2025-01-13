e73962db59bd681ad2ed1d123e121849014796cb
Andrew Prokhorenkov <aprokh@uchicago.edu>
2025-01-13 03:37:06 -0600

    test: add test for series endpoint

--- tests/test_agg_mds_tcia_adapter.py
+++ tests/test_agg_mds_tcia_adapter.py
@@ -6,7 +6,7 @@ from mds.agg_mds.adapters import get_metadata
 
 @respx.mock
 def test_get_metadata_tcia():
-    tcia_response = """
+    tcia_response_study = """
     [
         {
             "StudyInstanceUID": "study_id_1",
@@ -39,7 +39,7 @@ def test_get_metadata_tcia():
     ]
     """
 
-    field_mappings = {
+    field_mappings_study = {
         "_unique_id": "path:StudyInstanceUID",
         "commons": "TCIA",
         "study_title": "path:StudyDescription",
@@ -48,26 +48,94 @@ def test_get_metadata_tcia():
         "tags": [],
     }
 
+    tcia_response_series = """
+    [
+        {
+            "SeriesInstanceUID": "series_id_1",
+            "StudyInstanceUID": "study_id_1",
+            "Modality": "A",
+            "ProtocolName": "",
+            "SeriesDate": "1970-01-01 00:00:00.0",
+            "SeriesDescription": "",
+            "SeriesNumber": 1,
+            "Collection": "Collection1",
+            "PatientID": "",
+            "Manufacturer": "",
+            "ManufacturerModelName": "",
+            "ImageCount": 10,
+            "TimeStamp": "1970-01-01 00:00:00.0",
+            "LicenseName": "",
+            "LicenseURI": "",
+            "CollectionURI": "",
+            "FileSize": 100,
+            "DateReleased": "1970-01-01 00:00:00.0",
+            "StudyDesc": "",
+            "StudyDate": "1970-01-01 00:00:00.0",
+            "ThirdPartyAnalysis": ""
+        },
+        {
+            "SeriesInstanceUID": "series_id_2",
+            "StudyInstanceUID": "study_id_2",
+            "Modality": "B",
+            "ProtocolName": "",
+            "SeriesDate": "1970-01-01 10:00:00.0",
+            "SeriesDescription": "",
+            "BodyPartExamined": "",
+            "SeriesNumber": 2,
+            "Collection": "Collection2",
+            "PatientID": "",
+            "Manufacturer": "",
+            "ManufacturerModelName": "",
+            "SoftwareVersions": "",
+            "ImageCount": 20,
+            "TimeStamp": "1970-01-01 10:00:00.0",
+            "LicenseName": "",
+            "LicenseURI": "",
+            "CollectionURI": "",
+            "FileSize": 200,
+            "DateReleased": "1970-01-01 10:00:00.0",
+            "StudyDesc": "",
+            "StudyDate": "1970-01-01 10:00:00.0",
+            "ThirdPartyAnalysis": ""
+        }
+    ]
+    """
+
+    field_mappings_series = {
+        "_unique_id": "path:SeriesInstanceUID",
+        "study_id": "path:StudyInstanceUID",
+        "commons": "TCIA",
+        "study_title": "path:SeriesDescription",
+        "program_name": "path:Collection",
+        "image_count": "path:ImageCount",
+        "description": "",
+        "tags": [],
+    }
+
     respx.get("http://test/ok").mock(side_effect=httpx.HTTPError)
     assert (
-        get_metadata("tcia", "http://test/ok", filters=None, mappings=field_mappings)
+        get_metadata(
+            "tcia", "http://test/ok", filters=None, mappings=field_mappings_study
+        )
         == {}
     )
 
     respx.get("http://test/ok").mock(side_effect=Exception)
     assert (
-        get_metadata("tcia", "http://test/ok", filters=None, mappings=field_mappings)
+        get_metadata(
+            "tcia", "http://test/ok", filters=None, mappings=field_mappings_study
+        )
         == {}
     )
 
     respx.get(
         "http://test/ok",
-    ).mock(return_value=httpx.Response(status_code=200, content=tcia_response))
+    ).mock(return_value=httpx.Response(status_code=200, content=tcia_response_study))
 
     filters = {"size": 5}
 
     assert get_metadata(
-        "tcia", "http://test/ok", filters=filters, mappings=field_mappings
+        "tcia", "http://test/ok", filters=filters, mappings=field_mappings_study
     ) == {
         "study_id_1": {
             "_guid_type": "discovery_metadata",
@@ -88,6 +156,41 @@ def test_get_metadata_tcia():
                 "description": "TCIA data from collection: Collection2.",
                 "program_name": "Collection2",
                 "study_title": "Collection Two.",
+                "tags": [{"category": "program_name", "name": "Collection2"}],
+            },
+        },
+    }
+
+    respx.get(
+        "http://test/ok",
+    ).mock(return_value=httpx.Response(status_code=200, content=tcia_response_series))
+
+    assert get_metadata(
+        "tcia", "http://test/ok", filters=filters, mappings=field_mappings_series
+    ) == {
+        "series_id_1": {
+            "_guid_type": "discovery_metadata",
+            "gen3_discovery": {
+                "_unique_id": "series_id_1",
+                "study_id": "study_id_1",
+                "commons": "TCIA",
+                "study_title": "",
+                "program_name": "Collection1",
+                "image_count": "10",
+                "description": "",
+                "tags": [{"category": "program_name", "name": "Collection1"}],
+            },
+        },
+        "series_id_2": {
+            "_guid_type": "discovery_metadata",
+            "gen3_discovery": {
+                "_unique_id": "series_id_2",
+                "study_id": "study_id_2",
+                "commons": "TCIA",
+                "study_title": "",
+                "program_name": "Collection2",
+                "image_count": "20",
+                "description": "",
                 "tags": [{"category": "program_name", "name": "Collection2"}],
             },
         },
